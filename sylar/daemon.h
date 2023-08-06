352d9a638e060dad09dcabd891acada85d9f51a9
xjh19971110 <1244840409@qq.com>
2023-08-06 16:49:47 +0800

    添加守护进程模块

--- sylar/daemon.h
+++ sylar/daemon.h
@@ -13,6 +13,7 @@
 #include "singleton.h"
 
 namespace sylar {
+
 struct ProcessInfo {
     /// 父进程id
     pid_t parent_id = 0;
@@ -28,7 +29,20 @@ struct ProcessInfo {
     std::string toString() const;
 };
 
+typedef sylar::Singleton<ProcessInfo> ProcessInfoMgr;
 
-
+/**
+ * @brief 启动程序可以选择用守护进程的方式
+ * @param[in] argc 参数个数
+ * @param[in] argv 参数值数组
+ * @param[in] main_cb 启动函数
+ * @param[in] is_daemon 是否守护进程的方式
+ * @return 返回程序的执行结果
+ */
+int start_daemon(int argc, char** argv
+                 , std::function<int(int argc, char** argv)> main_cb
+                 , bool is_daemon);
 
 }
+
+#endif
